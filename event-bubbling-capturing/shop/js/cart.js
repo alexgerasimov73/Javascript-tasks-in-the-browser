4907b5ea533784d320d4f24e589a2a547c79a980
alexgerasimov73 <AG73@list.ru>
2018-05-23 02:31:17 +0300

    Добавлено реешение домашней работы

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,20 @@
 'use strict';
+
+let addButtons = document.getElementsByClassName('add-to-cart');
+
+function add(event) {
+  event.preventDefault();
+  addToCart({title: this.dataset.title, price: this.dataset.price});
+}
+
+function addMore() {
+  for (const button of addButtons) {
+    button.addEventListener('click', add);
+  }
+}
+
+for (const button of addButtons) {
+  button.addEventListener('click', add);
+}
+
+showMore.addEventListener('click', addMore);
--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,5 +1,16 @@
 'use strict';
 
 function handleTableClick(event) {
-  
+  if (event.target.dataset.propName && event.target.classList.contains('prop__name')) {
+    table.dataset.sortBy = event.target.dataset.propName;
+    if (step % 2 === 0) {
+      event.target.dataset.dir = 1;
+    } else {
+      event.target.dataset.dir = -1;
+    }
+    sortTable(event.target.dataset.propName, event.target.dataset.dir);
+    step++;
+  }
 }
+
+let step = 0;
